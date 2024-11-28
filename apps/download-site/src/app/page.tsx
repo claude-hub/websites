a52b9a18fd14245b4c1d127c68f1ce27707f5725
zhangyunpeng <zhangyunpeng@testbird.com>
2024-11-28 16:03:05 +0800

    feat: 🎸 test click

--- apps/download-site/src/app/page.tsx
+++ apps/download-site/src/app/page.tsx
@@ -1,11 +1,19 @@
 import Article from './_article';
+import Demo from '@/components/Demo';
 
 export default function Home() {
   return (
     <>
       <div>Hello World!</div>
-      <button id="click">Click me</button>
+      <button
+        id="click"
+        data-click-trace-action={'click_fullscreen'}
+        data-click-trace-params={JSON.stringify({ value: 'fullscreen' })}
+      >
+        Click me
+      </button>
       <a href="https://www.baidu.com" target='_blank'>baidu</a>
+      <Demo />
       {/* <iframe src="https://www.baidu.com" /> */}
       <Article />
     </>
