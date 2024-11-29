ebd2f1bf7a276ac8958d8154acf438f2f40c6c3a
zhangyunpeng <zhangyunpeng@testbird.com>
2024-11-29 16:22:54 +0800

    feat: 🎸 test after loading

--- packages/components/src/GTM.tsx
+++ packages/components/src/GTM.tsx
@@ -12,7 +12,16 @@ const GTMScript = ({ gtmId }: IGtmProps) => {
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${gtmId}'); console.log('gtm loaded: ', new Date().getTime(), new Date().getTime() - performance.timing.fetchStart)`,
+      })(window,document,'script','dataLayer','${gtmId}'); console.log('gtm loaded: ', new Date().getTime(), new Date().getTime() - performance.timing.fetchStart)
+      
+      
+       console.log('==gtm 加载后，立马上报==', window.dataLayer);
+          (window.dataLayer || []).push({
+            event: 'report_test111',
+            loading_time: new Date().getTime()
+          })
+
+      `,
       }}
     />
   );
