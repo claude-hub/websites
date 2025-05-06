29c8346b0db412fa05e12af8206292213f1b571a
zhangyunpeng <zhangyunpeng@testbird.com>
2025-05-06 09:46:55 +0800

    feat: 🎸 update sitemap & url

--- apps/online-games/src/pages/robots.txt.ts
+++ apps/online-games/src/pages/robots.txt.ts
@@ -3,11 +3,12 @@ import type { APIRoute } from 'astro';
 const getRobotsTxt = (sitemapURL: URL) => `
 User-agent: *
 Allow: /
+Disallow: /assets/
 
 Sitemap: ${sitemapURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
-  const sitemapURL = new URL('sitemap-index.xml', site);
-  return new Response(getRobotsTxt(sitemapURL));
+	const sitemapURL = new URL('sitemap-index.xml', site);
+	return new Response(getRobotsTxt(sitemapURL));
 };
