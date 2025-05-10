af8c8e4efab408c279100012c09d14976479947f
claude-hub <claudez1115@gmail.com>
2025-05-11 01:49:55 +0800

    style: 💄 UI 调整

--- apps/online-games/src/service/index.ts
+++ apps/online-games/src/service/index.ts
@@ -4,3 +4,5 @@ export const categories = [...new Set(gamesData.map((game) => game.category))];
 // export const games = [...gamesData].sort(() => 0.5 - Math.random());
 
 export const games = gamesData;
+
+export const popularGames = [...gamesData].sort(() => 0.5 - Math.random()).slice(0, 50);
