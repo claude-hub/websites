8278049899def31889e2518ee488b17262b459c1
claude-hub <claudez1115@gmail.com>
2025-05-14 21:48:45 +0800

    feat: 🎸 add games & back to top

--- apps/online-games/src/service/index.ts
+++ apps/online-games/src/service/index.ts
@@ -1,8 +1,10 @@
 import gamesData from './games.json';
 
-export const categories = [...new Set(gamesData.map((game) => game.category))];
-// export const games = [...gamesData].sort(() => 0.5 - Math.random());
+export const originGames = [...new Map(gamesData.map((game) => [game.id, game])).values()];
 
-export const games = gamesData;
+export const categories = [...new Set(originGames.map((game) => game.category))];
+export const games = [...originGames].sort(() => 0.5 - Math.random());
 
-export const popularGames = [...gamesData].sort(() => 0.5 - Math.random()).slice(0, 50);
+// export const games = gamesData;
+
+export const popularGames = [...originGames].sort(() => 0.5 - Math.random()).slice(0, 50);
